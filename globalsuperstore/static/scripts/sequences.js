402af2e5f6a75e2b33ac446c7bfbe0ebdabc7a7d
Hari Natamai <hari.natamai@gmail.com>
2020-05-09 11:48:04 -0400

    WIP Hari

--- globalsuperstore/static/scripts/sequences.js
+++ globalsuperstore/static/scripts/sequences.js
@@ -96,7 +96,7 @@ var arc = d3.svg.arc()
 
 // Use d3.text and d3.csv.parseRows so that we do not need to have a header
 // row, and can receive the csv as an array of arrays.
-d3.text("./global-sales1.csv", function(text) {
+d3.text("global-sales1.csv", function(text) {
   var csv = d3.csv.parseRows(text);
   console.log(csv);
   var json = buildHierarchy(csv);
@@ -152,7 +152,7 @@ function mouseover(d) {
   }
   
   // use currency.js to convert float to currency with $ symbol and comma separated.
-  sales = currency(d.value, { formatWithSymbol: true }).format();
+  sales = currency(d.value, { formatWithSymbol: true, precision: 0 }).format();
 
   d3.select("#percentage")
     //   .text(percentageString);
